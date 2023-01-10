06d37545fdc13cf6b6392c95de5fa22ec98e316d
unknown <merogit@hotmail.com>
2023-01-09 21:24:38 -0500

    1st and 2nd exercise

--- part2/courseinfopart2e2/src/App.js
+++ part2/courseinfopart2e2/src/App.js
@@ -1,3 +1,10 @@
+const Total = (props)=>{
+  const {arraytotal} = props
+  return(
+        <div><b>total of {(arraytotal.reduce((a,v)=> a=a+v.exercises,0))} exercises</b></div>
+    )
+}
+
 const Part =(props)=>{
   const {part}=props
   return(
@@ -26,6 +33,7 @@ const Course = (props)=>{
     <div>
       <Header header ={course.name} />
       <Content content ={course.parts} />
+      <Total arraytotal={course.parts}/>
     </div>
   )
 }
